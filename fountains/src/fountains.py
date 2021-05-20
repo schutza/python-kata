d556d74f310fc89d4b5c712e84ba08b551cc3c3f
Alexander Schutz <alexander.schutz@textability.com>
2021-05-20 19:47:02 +0100

    initial commit with approach

---
+++ fountains/src/fountains.py
@@ -0,0 +1,18 @@
+from typing import List
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'fountainActivation' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY locations as parameter.
+#
+
+def fountain_activation(locations: List[int]) -> int:
+    # Write your code here
+    pass
